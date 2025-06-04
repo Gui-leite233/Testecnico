68fa05165437a909efc55a37f211462f7d9054dd
Gui-leite233 <89623564+Gui-leite233@users.noreply.github.com>
2025-06-04 16:39:26 -0300

    Projeto quase concluído

--- app/Controllers/ProcessoPagamentoController.php
+++ app/Controllers/ProcessoPagamentoController.php
@@ -4,11 +4,139 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
+use App\Models\PedidosModel;
+use App\Models\LojasGateway;
+use App\Models\PedPagModel;
+use App\Libraries\PagCompletoGateway;
 
 class ProcessoPagamentoController extends BaseController
 {
+    protected $pedidosModel;
+    protected $pedpagModel;
+    protected $lojasGatewayModel;
+    protected $pagCompletoGateway;
+
+
+    public function __construct()
+    {
+        $this->pedidosModel = new PedidosModel();
+        $this->pedpagModel = new PedPagModel();
+        $this->lojasGatewayModel = new LojasGateway();
+        $this->pagCompletoGateway = new PagCompletoGateway();
+    }
+
     public function index()
     {
-        //
+
+        try {
+            $pedidoProcesso = $this->buscaPedido();
+
+            $result = [];
+
+            return $this->response->setJSON([
+                'success' => true,
+                'message' => 'Processamento concluido',
+                'pedidos_processamento' => count($result),
+                'resultados' => $result
+            ]);
+
+        } catch (\Exception $e) {
+            log_message('error', 'Erro no processamento: ' . $e->getMessage());
+
+            return $this->response->setStatusCode(500)->setJSON([
+                'success' => false,
+                'message' => 'Erro interno',
+                'error' => $e->getMessage()
+            ]);
+        }
+    }
+
+
+    private function buscaPedido()
+    {
+        $builder = $this->pedidosModel->db->table('pedidos p');
+        $builder->select('p.*, pp.*, c.nome as cliente_nome, c.cpf_cnpj, c.email, c.data_nasc')->join('pedidos_pagamentos pp', 'p.id = pp.id_pedido')->join('lojas_gateway lg', 'p.id_loja=pp.id_pedido')->join('clientes c', 'p.id_cliente=c.id')->where('ld.id_gateway', 1)->where('pp.id_formatopagto', 3)->where('p.id_situacao', 1)->where('pp.retorno_intermediador IS NULL');
+
+        return $builder->get()->getResult();
+    }
+
+
+    private function processaPedido($pedido){
+        try {
+            $dadosClient = $this->preparaDados($pedido);
+            $dadosPag = $this->preparaDadosPag($pedido, $dadosClient);
+            $retornoGwy = $this->pagCompletoGateway->processaTransacao($dadosPag);
+            $this->atualizaStatus($pedido, $retornoGwy);
+            $this->salvaRetornoInter($pedido['id'], $retornoGwy);
+
+            return [
+                'pedido_id'=>$pedido['id'],
+                'status'=>'processado',
+                'retorno_gateway'=>$retornoGwy
+            ];
+        } catch (\Exception $e) {
+             log_message('error', "Erro ao processar pedido {$pedido['id']}: " . $e->getMessage());
+            
+            return [
+                'pedido_id' => $pedido['id'],
+                'status' => 'erro',
+                'erro' => $e->getMessage()
+            ];
+        }
+    }
+
+
+    private function preparaDados($pedido){
+        return[
+            'exeternal_id'=>(string)$pedido['id_cliente'],
+            'name'=>$pedido['cliente_nome'],
+            'type'=>'individual',
+            'email'=>$pedido['email'],
+            'document'=>[
+                'type'=>'cpf',
+                'number'=>$pedido['cpf_cpnj']
+            ],
+            'birthday'=>$pedido['data_nasc']
+        ];
+    }
+
+
+    private function preparaDadosPag($pedido, $dadosClient) {
+        $venc=$pedido['vencimento'];
+        $vencFomtt = str_replace('-', '', $venc);
+
+        return [
+            'external_order_id' => $pedido['id'],
+            'amount'=>(float)$pedido['valor_total'],
+            'card_numer'=>$pedido['num_cartao'],
+            'card_cvv'=>(string)$pedido['codigo_verificador'],
+            'card_expiration_date'=>$vencFomtt,
+            'card_holder_name'=>$pedido['nome_portador'],
+            'customer'=>$dadosClient
+        ];
+    }
+
+    private function atualizaStatus($pedido, $retornoGateway) {
+        $novoStatus = 1;
+
+        if(isset($retornoGateway['error']) && $retornoGateway['Error']==false){
+            if(isset($retornoGateway['Transaction_code'])&&$retornoGateway['Transaction_code']==00){
+                $novoStatus=2;//deu boa
+            }
+        } else{
+            $novoStatus = 3;//deu ruim
+        }
+
+        $this->pedidosModel->update($pedido['id'], ['id_situacao'=>$novoStatus]);
+    }
+
+
+    private function salvaRetornoInter($pedido, $retornoGateway) {
+        $retornoJson = json_encode($retornoGateway);
+        $ProcessDate = date('Y-m-d H:i:s');
+
+        $builder = $this->pedpagModel->db->table('pedidos_pagamentos');
+        $builder->where('id_pedido', $pedido);
+        $builder->update(['update_intermediador'=> $retornoJson, 'data_processamento'=>$ProcessDate]);
     }
 }
---
+++ app/Libraries/PagCompletoGateway.php
@@ -0,0 +1,204 @@
+<?php
+
+namespace App\Libraries;
+
+use CodeIgniter\HTTP\CURLRequest;
+use finfo;
+use PhpParser\Builder\Function_;
+use PhpParser\Node\Expr\New_;
+
+class PagCompletoGateway{
+    private $baseURL;
+    private $endpoint;
+    private $accessToken;
+    private $timeout;
+
+
+    public function __construct(){
+        $this->baseURL = 'https://apiinterna.ecompleto.com.br';
+        $this->endpoint = '/exams/processTransaction';
+        $this->accessToken = env('PAGCOMPLETO_ACCESS_TOKEN', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjI2ODQsInN0b3JlSWQiOjE5NzksImlhdCI6MTc0ODU0NTIwNiwiZXhwIjoxNzQ5NDA5MjA2fQ.pngOSO40bI67Q1bCwWc_SFdIuBRhDQKww2DyxfhTKqo');
+        $this->timeout = 30;
+    }
+
+
+    public function transacao(array $dadosPagamento){
+        try{
+            $this -> validateDadosTr($dadosPagamento);
+
+            $url = $this->baseURL . $this->endpoint . '?accesstoken=' . $this->accessToken;
+
+            $curl = curl_init();
+
+            curl_setopt_array($curl, [
+                CURLOPT_URL => $url,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_TIMEOUT => $this->timeout,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => json_encode($dadosPagamento),
+                CURLOPT_HTTPHEADER => [
+                    'content-Type: applications/json',
+                    'Accept: application/json'
+                ],
+                CURLOPT_SSL_VERIFYPEER => true,
+                CURLOPT_SSL_VERIFYHOST => 2
+            ]);
+
+
+            $response = curl_exec($curl);
+            $http = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+            $error = curl_error($curl);
+
+            curl_close($curl);
+
+            if($error){
+                throw new \Exception("Erro: " . $error);
+            }
+
+            if($http!=200){
+                throw new \Exception("Gateway retorno: " . $http);
+            }
+
+            $retorno = json_decode($response, true);
+
+            if(json_last_error()!==JSON_ERROR_NONE){
+                throw new \Exception("Erro na resposta do gateway: " . json_last_error_msg());
+            }
+
+            $this->logTransacao($dadosPagamento, $retorno);
+
+            return $retorno;
+        } catch(\Exception $e){
+            log_message('erro', 'Erro PagCompletoGateway:' . $e->getMessage());
+
+            return [
+                'Error' => true,
+                'Transaction_code' => '99',
+                'Message' => 'Erro na comunicacao: ' . $e->getMessage()
+            ];
+        }
+    }
+
+
+    private function validateDadosTr(array $dados){
+        $obg = [
+            'external_order_id',
+            'amount',
+            'card_number',
+            'card_cvv',
+            'card_expiration_date',
+            'card_holder_name',
+            'customer'
+        ];
+
+        foreach($obg as $campo){
+            if(!isset($dados[$campo]) || empty($dados[$campo])){
+                throw new \Exception("campo obrigatoroio: " . $campo);
+            }
+        }
+
+        if (!isset($dados['customer']['external_id']) || !isset($dados['customer']['name']) || !isset($dados['email']) || !isset(['customer']['documents'])) {
+            throw new \Exception("dados incompletos");
+            
+        }
+
+        if (!is_numeric($dados['amount'] || $dados['amount'] <=0)) {
+            throw new \Exception("Valor inválido");
+        }
+
+        if (!$this->validateNum($dados['card_number'])) {
+            throw new \Exception("Número inválido");
+        }
+
+        if (!preg_match('/^\d{3,4}$/', $dados['card_cvv'])) {
+            throw new \Exception("cvv invalido");
+        }
+
+        if(!$this->validateDataVen($dados['card_expriration_date'])){
+            throw new \Exception("data de vencimento invalida");
+        }
+
+
+
+    }
+
+    private function validateNum($num) {
+        
+        $num = preg_replace('/\D/', '', $num);
+
+        if(strlen($num) || strlen($num)>19){
+            return false;
+        }
+
+        $sum = 0;
+        $alt = false;
+
+        //algoritmo de luhn, achei o código e mudei as informações. Conceito interessante e novo.
+        for ($i = strlen($num) - 1; $i >= 0; $i--) {
+            $dgt = (int)$num[$i];
+            
+            if ($alt) {
+                $dgt *= 2;
+                if ($dgt > 9) {
+                    $dgt -= 9;
+                }
+            }
+            
+            $sum += $dgt;
+            $alt = !$alt;
+        }
+
+        return ($sum % 10)===0;
+    }
+
+    private function logTransacao(array $dados, array $retorno){
+        $logdata = [
+            'timestamp'=>date('Y-m-d H:i:s'),
+            'pedido_id' => $dados['external_order_id'] ?? 'N/A',
+            'valor' => $dados['amount']??'N/A',
+            'cartao_final'=>isset($dados['card_number']) ? '**** **** **** ' . substr($dados['card_number'], -4) : 'N/A',
+            'status_transacao' => $retorno['Transaction_code'] ?? 'N/A',
+            'mensagem' => $retorno['Message'] ?? 'N/A',
+            'erro' => $retorno['Error'] ?? 'N/A'
+        ];
+
+        log_message('info', 'Transação PagCompleto: ' . json_encode($logdata));
+    }
+
+    private function validateDataVen($dados){
+        $dados = preg_replace('/\D/', '', $dados);
+
+
+        if(strlen($dados)!==4 && strlen($dados)!==6){
+            return false;
+        }
+
+        if(strlen($dados)==4){
+            $mes = (int) substr($dados, 0, 2);//MM
+            $ano = (int) ('20' . substr($dados, 2, 4));//YY
+        } else{
+            $mes = (int)substr($dados, 0, 2);//MM
+            $ano = (int)substr($dados, 2, 4);//YYYY
+        }
+
+        if($mes < 1 || $mes > 12){
+            return false;
+        }
+
+        $hj = new \DateTime();
+        $venc = new \DateTime($ano . '-' . str_pad($mes, 2, '0', STR_PAD_LEFT) . '-01');
+        $venc->modify('ultimo dia');
+
+        return $venc;
+    }
+
+
+
+    public function setTimeout($timeout){
+        $this->timeout = (int)$timeout;
+    }
+    public function setAccessToken($token){
+        $this->accessToken = $token;
+    }
+
+}
