6206be100ef746c931b4770748a90138cb2392b4
Gui-leite233 <89623564+Gui-leite233@users.noreply.github.com>
2025-06-04 16:44:28 -0300

    Rotas

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,11 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+
+$routes->group('pagamentos', function($routes){
+    $routes->get('processar'. 'ProcessoPagamentoController::index');
+    
+    
+
+});
